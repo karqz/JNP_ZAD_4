bc863eb127220d67ecea37bf7909c149900e5fd3
rog1gor <52384567+rog1gor@users.noreply.github.com>
2021-12-04 20:55:40 +0100

    przechodzi testy

--- member.h
+++ member.h
@@ -8,74 +8,82 @@ template<std::size_t v>
 concept CompletedExpeditions = v < 25;
 
 constexpr uint32_t fibonacci(uint32_t n) {
-    return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    uint32_t fib[n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (std::size_t i = 2; i <= n; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
+
+    return fib[n];
 }
 
-template<ValueType T, bool IsArmed> 
+template<ValueType T, bool IsArmed>
 class Adventurer {};
 
 template<ValueType T>
 class Adventurer<T, true> {
-    public:
-        using strength_t = uint32_t;
+public:
+    using strength_t = uint32_t;
 
-        constexpr static bool isArmed = true;
+    constexpr static bool isArmed = true;
 
-        constexpr Adventurer(strength_t strength):strength(strength),value(0) {};
+    constexpr Adventurer(strength_t strength):strength(strength),value(0) {};
 
-        constexpr strength_t getStrength() const {
-            return strength;
+    constexpr strength_t getStrength() const {
+        return strength;
+    }
+
+    template<bool IsTrapped>
+    constexpr void loot(Treasure<T, IsTrapped> &&treasure) {
+        if (!IsTrapped) {
+            value += treasure.getLoot();
         }
-
-        template<bool IsTrapped>
-        constexpr void loot(Treasure<T, IsTrapped> &&treasure) {
-            if (!IsTrapped) {
-                value += treasure.getLoot();
-            }
-            else if (strength > 0) {
-                value += treasure.getLoot();
-                strength /= 2;
-            }
+        else if (strength > 0) {
+            value += treasure.getLoot();
+            strength /= 2;
         }
+    }
 
-        constexpr T pay() {
-            T to_pay = value;
-            value = 0;
-            return to_pay;
-        }
+    constexpr T pay() {
+        T to_pay = value;
+        value = 0;
+        return to_pay;
+    }
 
-    private:
-        strength_t strength;
-        T value;
+private:
+    strength_t strength;
+    T value;
 };
 
 template<ValueType T>
 class Adventurer<T, false> {
-    public:
-        using strength_t = uint32_t;
+public:
+    using strength_t = uint32_t;
 
-        constexpr static bool isArmed = false;
+    constexpr static bool isArmed = false;
 
-        constexpr Adventurer():value(0) {};
+    constexpr Adventurer():value(0) {};
 
-        template<bool IsTrapped>
-        constexpr void loot(Treasure<T, IsTrapped> &&treasure) {
-            if (!IsTrapped) {
-                value += treasure.getLoot();
-            }
-            else if (strength > 0) {
-                value += treasure.getLoot();
-            }
+    template<bool IsTrapped>
+    constexpr void loot(Treasure<T, IsTrapped> &&treasure) {
+        if (!IsTrapped) {
+            value += treasure.getLoot();
         }
+    }
 
-        constexpr T pay() {
-            T to_pay = value;
-            value = 0;
-            return to_pay;
-        };
+    constexpr T pay() {
+        T to_pay = value;
+        value = 0;
+        return to_pay;
+    };
 
-    private:
-        T value;
+private:
+    T value;
 };
 
 template<ValueType T>
@@ -84,33 +92,33 @@ using Explorer = Adventurer<T, false>;
 
 template<ValueType T, std::size_t CompletedExpeditions>
 class Veteran {
-    public:
-        using strength_t = uint32_t;
+public:
+    using strength_t = uint32_t;
 
-        constexpr static bool isArmed = true;
+    constexpr static bool isArmed = true;
 
-        constexpr Veteran():strength(fibonacci(CompletedExpeditions)),value(0) {};
+    constexpr Veteran():strength(fibonacci(CompletedExpeditions)),value(0) {};
 
-        constexpr strength_t getStrength() const {
-            return strength;
+    constexpr strength_t getStrength() const {
+        return strength;
+    }
+
+    template<bool IsTrapped>
+    constexpr void loot(Treasure<T, IsTrapped> &&treasure) {
+        if ((IsTrapped && strength > 0) || !IsTrapped) {
+            value += treasure.getLoot();
         }
-        
-        template<bool IsTrapped>
-        constexpr void loot(Treasure<T, IsTrapped> &&treasure) {
-            if (strength > 0) {
-                value += treasure.getLoot();
-            }
-        }
+    }
 
-        constexpr T pay() {
-            T to_pay = value;
-            value = 0;
-            return to_pay;
-        };
+    constexpr T pay() {
+        T to_pay = value;
+        value = 0;
+        return to_pay;
+    };
 
-    private:
-        strength_t strength;
-        T value;
+private:
+    strength_t strength;
+    T value;
 };
 
 #endif //__MEMBER__
